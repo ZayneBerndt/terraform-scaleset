60a2af1f55ae92f8092e1ddc95aedaa0fec6e779
Zayne  Berndt <berndt.zayne@gmail.com>
2018-10-25 18:12:34 +0100

    vm scale set and packer image

--- variables.tf
+++ variables.tf
@@ -1,6 +1,15 @@
+variable "subscription_id" {}
+
+variable "admin_user" {}
+variable "admin_password" {}
+
 variable "location" {
   description = "The location where resources are created"
   default     = "East US"
 }
 
-variable "subscription_id" {}
+variable "application_port" {
+    description = "The port that you want to expose to the external load balancer"
+    default     = 80
+}
+
